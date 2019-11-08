b888baf7043497e5f3f621796bf2ac0ed6ac5347
Joseph Miles <josephmiles2015@gmail.com>
2019-11-08 14:25:28 -0800

    Added output file command line argument.
    Now we can output files!

--- src/pilcrow.py
+++ src/pilcrow.py
@@ -9,7 +9,7 @@
 # formatting language and markup system that gives authors maximum control over
 # their document design. Write once, display everywhere.
 
-import sys, argparse
+import sys, argparse, os
 
 PILCROW = "¶"
 
@@ -17,13 +17,29 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("input")
+    parser.add_argument("output", nargs="?")
 
     args = parser.parse_args(sys.argv[1:])
 
-    if args.input:
-        manuscript = None
 
-        with open(args.input, "r") as in_file:
-            manuscript = in_file.read()
+    manuscript = None
 
-        print(manuscript)
+    with open(args.input, "r") as in_file:
+        manuscript = in_file.read()
+
+
+    out_filepath = None
+
+    # If the user didn't specify an output file, we'll just infer the output
+    # filename from the given input filename.
+    if args.output:
+        out_filepath = args.output
+
+    else:
+        f = os.path.basename(args.input)
+        # todo(jrm): Later we will get the output file extension from the user,
+        # but for now we'll just stick with outputting .txt files.
+        out_filepath = os.path.splitext(f)[0] + ".txt"
+
+    with open(out_filepath, "w") as out_file:
+        out_file.write(manuscript)
